2469826aa136e81a60c0ddf7d4363fedd61fe5cc
Nana Amfo-Brobbey <nana.amfobrobbey@gmail.com>
2024-11-08 15:58:26 +0000

    Creating a solid draft of the user system - both creating and logging a user

--- C++/LibraryManagement/LibraryManager.cpp
+++ C++/LibraryManagement/LibraryManager.cpp
@@ -13,22 +13,24 @@ Member::Member(string name, int age, string membershipID, string borrowedBooks[]
     borrowedBooks = borrowedBooks;
 }
 
-string menuInput(){
-    string input = "";
+char menuInput(){
+    char input;
     cout << "Please select a number for the option of: " << endl;
     cout << "1- Login" << endl << "2- Register" << endl;
     cout << "3- Borrow a book" << endl << "4- Exit" << endl;
     cout << "Option: " << endl;
     //Taking the string input which represents the choice that the user has made
-    while (input != "1" && input != "2" && input != "3" && input != "4") cin >> input;
+    while (input != '1' && input != '2' && input != '3' && input != '4') cin >> input;
     return input;
 }
 
-string findMember(ofstream &textFile, string &memberID, string &password){
-    //Should return the newLine storing all of the values of this string
+Member findMember(string &memberID, string &password){
+    ifstream reader("memberships.txt");
+
+    //Should return the new Line storing all of the values of this string
     string newline = "";
-    getline(textFile, newline);
-    if(!textFile.eof()){
+    getline(reader, newline);
+    if(!reader.eof()){
         //Split the string into an array of strings, separating each string with commas like a CSV file
         stringstream memberDetails(newline);
         string membershipID = " ";
@@ -46,26 +48,35 @@ string findMember(ofstream &textFile, string &memberID, string &password){
                 memberDetails.clear(); //Clears and empties the data stored in memberDetails - could be optimised later on perhaps
                 stringstream foundMemberDetails(newline);
                 string basicValues[3]; //I will store the name, age and membershipID in this
-                sting tempString = "";
+                string tempString = "";
                 for(int i=0;i<3;i++){
                     getline(foundMemberDetails, basicValues[i], ",");
                 }
                 //This is the password which we aren't storing
                 getline(foundMemberDetails, tempString, ",");
+
+                //From this point onwards, we should just be fetching the borrowed books of this user
+                int arrayLength = 0;
+                string borrowedBooks[10]; //Could use arraycpy() to double array size when threatened to fill up completely
+                //Use an arraycpy to copy values into a smaller array of the right size;
+                while(getline(foundMemberDetails, tempString, ",")){
+                    borrowedBooks[arrayLength++] = tempString;
+                }
+
+                //stoi converts strings to integers
+                Member foundMember = new Member(basicValues[0], stoi(basicValues[1]), basicValues[2], thisPassword, borrowedBooks);
+                return foundMember;
             }
         }
     }
+    //If we fail to find the member, then I'll return an empty Member with no defined variables
+    Member emptyBoi = new Member();
+    return emptyBoi;
 }
 
 int main(){
     ofstream UserDataBase("memberships.txt");
-    //I'm thinking of storing the details of each member in a single line in a CSV format
-    //Need to figure out how to split values using a delimiter
-        //This link could be helpful: https://www.geeksforgeeks.org/how-to-split-string-by-delimiter-in-cpp/
-        /*Could also store books on this line and check for the total
-         number of books by looking at the length of the array past 3 - 
-         0-3 store the name, age, ID and password of the user*/
-    string choice = menuInput();
+    char choice = menuInput();
     switch(choice){
         case "1":
             //Logging in as a user
@@ -76,33 +87,59 @@ int main(){
             cin >> membershipID;
             cout << endl << "Please enter your password: ";
             cin >> password;
-                //Make sure that we find a match in the .txt file
-            
-            //Print out all of the details of this member object as shown in the PDF
+            //Make sure that we find a match in the .txt file
+                //Could maybe return an empty Member object if we fail to find this user
+            Member returningUser = findMember(membershipID, password);
+            //Print a "Welcome message first"
+            cout << "Welcome " << returningUser.getName() << "!" << endl;
+            //Then print out the object
+            cout << returningUser;
             break;
         case "2":
             //Registering a new user
             string name;
             string membershipID;
             string password;
-            string[] borrowedBooks; 
             int age;
+            int booksToBorrow = 0;
+            //Get the user's information here
             cout << "Please enter your name: ";
             cin >> name;
+
             cout << endl << "Please enter your age: ";
-            cin >> age;
+            cin >> age; 
+
             cout << endl << "Please enter your 6-digit membership ID: ";
             cin >> membershipID;
-            cin << endl << "Please enter your password";
+
+            cout << endl << "Please enter your password: ";
             cin >> password;
-            UserDataBase << name << "," << to_string(age) << "," << membershipID << "," << password << ",";
-            //Don't forget to ask the user which books they would want to borrow
-                //Store that in the line as well
+
+            cout << "Please tell us how many books you are going to borrow initially? ";
+            cin >> booksToBorrow;
+            //Dynamically creating an array of size booksToBorrow
+            string* borrowedBooks = new string[booksToBorrow];
+            //As I know the number of books which they are borrowing, I could theoretically also store this in the Member object
+            UserDataBase << name << "," << age << "," << membershipID << "," << password;
+            for(int i=0;i<booksToBorrow<i++){
+                string bookName;
+                string bookAuthor;
+                //Ask them for the name of the book to borrow
+                cout << "Please enter the name of the book you are borrowing: ";
+                cin >> bookName;
+                cout << endl << "Please enter the author of this book: ";
+                cin >> bookAuthor;
+                    //Could ask them first for the book name and user the \" to show the quotations around it
+                    //Then ask them for the author and stick a "by" in-between them for nice visualisation
+                //Replace this space placeholder with that name of the book
+                UserDataBase << "," << "\"" << bookName << "\"" << " by " << bookAuthor; 
+            }
+            UserDataBase << endl; //This should signify to the program that this is all of the data for this user
             break;
         case "3":
             //Borrowing a book
             break;
         case "4":
-            break;  
+            exit();  
     }
 }
--- C++/LibraryManagement/Member.h
+++ C++/LibraryManagement/Member.h
@@ -8,13 +8,37 @@ using namespace std;
 class Member{
     public:
         //Trying a one-line constructor like used in Lab 2
+        Member();
         Member(string name, int age, string membershipID, string[] borrowedBooks);
+
+        ostream& operator<<(ostream &os, const Member &other)
+        {
+            os << "Your age is " << other.age << endl
+            << "Your membership ID is " << other.membershipID << "." << endl
+            "Your borrowed books are: " << endl;
+   
+            if(arr != 0){
+                int size = sizeof(borrowedBooks) / sizeof(borrowedBooks[0]);
+                for(int i=0;i<size<i++){
+                    //Could come back to this later to manually add the quotation marks to the book titles
+                    os << " - " << borrowedBooks[i] << endl;
+                }
+            }
+
+            return os;
+        }
+
+        void getName(){
+            return name;
+        }
 
     private:
         string name;
         int age;
         string membershipID;
         string[] borrowedBooks;
+
+    
         
 }
 
