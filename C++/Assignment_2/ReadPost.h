9ca6e133d6f218695deadfbbdfc256da47d940cf
nana <nana.amfobrobbey@gmail.com>
2025-01-04 09:34:27 +0000

    Attempting first make

--- C++/Assignment_2/ReadPost.h
+++ C++/Assignment_2/ReadPost.h
@@ -108,7 +108,7 @@ public:
 
   //This function will automatically moderate all of the posts and update the text file accordingly
   void moderateAllPosts(string blacklistName, string postFileName){
-    vector< multimap<string, string> > it;
+    multimap<string, string>::iterator it;
     for(it = posts.begin(); it != posts.end(); it++){
       moderatePost(*it, blacklistName, postFileName);
     }
