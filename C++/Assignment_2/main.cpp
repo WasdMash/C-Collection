a061dda199af135ba7162ef4e2440a0e086b8134
NewDeveloper911 <nanabrobbey@aaess.sch.ae>
2025-01-11 18:04:51 +0000

    Debug messages

--- C++/Assignment_2/main.cpp
+++ C++/Assignment_2/main.cpp
@@ -234,9 +234,12 @@ void userOptions(User &currentUser, ReadPosts& postManager) {
             //Should probably around this point run the moderation function on this text
             pair<const string, string>& addedPost =  postManager.addPost(currentUser.getRegNo(), newPost, postFileName);
             currentUser.addScore(100, addedPost.second); //The default score for each post should be 100 before moderation
+            cout << "Printed a score" << endl;
             postManager.moderatePost(addedPost, blacklistName);
+            cout << "Moderating the post rn" << endl;
             postManager.updateTextFile(postFileName);
-            
+            cout << "Text file should be updarted now" << endl;
+
             currentUser.updateScores(); //Doesn't exist yet
             //Now, I should take the values from this current user and use them to update the values in the text file
             updateUserDatabase(postManager);
