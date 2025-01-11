72645116be304ac30b65ed8afc2653b449c1dabe
NewDeveloper911 <nanabrobbey@aaess.sch.ae>
2025-01-11 18:30:37 +0000

    Still same issue

--- C++/Assignment_2/ReadPost.cpp
+++ C++/Assignment_2/ReadPost.cpp
@@ -239,9 +239,10 @@ void ReadPosts::updateTextFile(string postFileName){
         }  
     }
     //Renaming the new file to be the same as the old, effectively updating the same file
-    close();
-    newDatabase.close();
+    
     try{
+        postfile.close();
+        newDatabase.close();
         remove(postFileName.c_str());
         rename("temp.txt", postFileName.c_str());
     }
