e54746665d10a03e1a8302502f69714a91323e23
NewDeveloper911 <nanabrobbey@aaess.sch.ae>
2025-01-11 12:34:15 +0000

    Lol, forgot a getter there

--- C++/Assignment_2/Manager.cpp
+++ C++/Assignment_2/Manager.cpp
@@ -6,3 +6,8 @@ using namespace std;
 // Constructor for the Manager class
 Manager::Manager(const string &name, int regNo) : Person(name), regNo(regNo){}
 
+// Getter for registration number
+int Manager::getRegNo() const{
+    return regNo;
+}
+
