c178e10123afc26501195fb5236ef33f3598b290
NewDeveloper911 <nanabrobbey@aaess.sch.ae>
2025-01-13 01:23:04 +0000

    I think constexpr remains as user, which could be annoying

--- C++/Assignment_2/main.cpp
+++ C++/Assignment_2/main.cpp
@@ -93,12 +93,7 @@ T login(string &username, ReadPosts& postManager) {
 
     //Let's check if our manager exists
     if (!managerName->empty()) {
-        //Checking if the class type passed into the template function is the same as Manager
-        if constexpr (is_same_v<T, Manager>) {
-            return Manager(*managerName, *managerID);
-        } else {
-            throw runtime_error("Requested type does not match the identified user type");
-        }
+        return Manager(*managerName, *managerID);
     }
 
     //If not, then let's delete the dynamic variables
